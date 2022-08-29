2afcb20aca72be71f0ccaf3ff7d062cfc9036dce
Leandro <freire.leandro@hotmail.com>
2022-08-29 17:43:12 -0300

    secon commit

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div>
+      Card Component
+    </div>
+    )
 };
